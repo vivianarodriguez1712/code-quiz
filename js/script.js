492dac7bf09bcb15a49ba4a8c2e21ea375d716c7
viviana rodriguez <vivianaprodriguez@live.com>
2022-06-02 01:42:47 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var startBtn = document.getElementById("start-Btn");
+
+function startQuiz() {
+console.log("it worked");
+}
+startBtn.addEventListener("click", startQuiz);
+
+//timer section
+
+var startingMinutes= 10;
+let time = startingMinutes * 60;
+
+const countdownEl = document.getElementById("countdown");
+
+setInterval(setCoutndown, 1000);
+
+function setCoutndown() {
+   var minutes = Math.floor(time / 60);
+   const seconds = time % 60;
+
+   countdownEl.innerHTML = '${minutes}: ${seconds}';
+   time--;
+}
