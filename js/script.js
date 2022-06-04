d8656b761948a727a77897f3ace8fe75b2d0c3e6
viviana rodriguez <vivianaprodriguez@live.com>
2022-06-04 02:49:46 -0400

    added quiz questions and buttons

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 var startBtn = document.getElementById("start-Btn");
 
+var question1 = ["strings","booleans","alerts","numbers"];
+let index = question1.indexOf("booleans", 1);
+
+var question2= ["commas","curly brackets","quotes","parenthesis"];
+let index = question2.indexOf("curly brackets", 2);
+
+var question3= ["JavaScript","terminal/bash","for loops","console.log"];
+let index = question3.indexOf("for loops", 3);
+
+var question4= ["quotes","curly brackets","parenthesis","square brackets"];
+let index = question4.indexOf("quotes", 4);
+
+var question5= ["numbers and strings","other arrays","booleans","all of the above"];
+let index = question5.indexOf("booleans", 5);
+
 function startQuiz() {
 console.log("it worked");
 setInterval(setCoutndown, 1000);
 }
 startBtn.addEventListener("click", startQuiz);
+
+
 
 //timer section
 
@@ -20,4 +37,4 @@ function setCoutndown() {
    countdownEl.innerHTML = `${minutes}: ${seconds}`;
    time--;
 }
-
+// end timer section
