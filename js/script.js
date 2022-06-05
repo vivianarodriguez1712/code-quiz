b0b125f195cce079f40a40495674e6fb004fbe5a
viviana rodriguez <vivianaprodriguez@live.com>
2022-06-04 21:58:39 -0400

    added else if's

--- js/script.js
+++ js/script.js
@@ -51,19 +51,19 @@ btnQ5A3.addEventListener("click", answerQuestions);
 btnQ5A4.addEventListener("click", answerQuestions);
 
 var question1 = ["strings","booleans","alerts","numbers"];
-var index1 = question1.indexOf("booleans");
+var index1 = "booleans";
 
 var question2= ["commas","curly brackets","quotes","parenthesis"];
-var index2 = question2.indexOf("curly brackets");
+var index2 = "curly brackets";
 
 var question3= ["JavaScript","terminal/bash","for loops","console.log"];
-var index3 = question3.indexOf("for loops");
+var index3 = "for loops";
 
 var question4= ["quotes","curly brackets","parenthesis","square brackets"];
-var index4 = question4.indexOf("quotes");
+var index4 = "quotes";
 
 var question5= ["numbers and strings","other arrays","booleans","all of the above"];
-var index5 = question5.indexOf("booleans")
+var index5 = "booleans";
 
 function answerQuestions(event) {
    var btn= event.target;
@@ -73,11 +73,37 @@ function answerQuestions(event) {
    console.log(questionID);
 
 
-   if (answered === index1 || answered === index2 || answered === index3 || answered === index4){
-       console.log(correct)
+   if (questionID === "Q1") {
+    if (answered === index1) {
+        alert("Correct!");
+    } else {
+        alert("Incorrect!");
+    }
+} else if (questionID === "Q2") {
+    if (answered === index2) {
+        alert("Correct!");
+    } else {
+        alert("Incorrect!");
+    }
+} else if (questionID === "Q3") {
+    if (answered === index3) {
+        alert("Correct!");
+    } else {
+        alert("Incorrect!");
+    }
+} else if (questionID === "Q4") {
+    if (answered === index4) {
+        alert("Correct!");
+    } else {
+        alert("Incorrect!");
+    }
+} else if (questionID === "Q5") {
+    if (answered === index5) {
+        alert("Correct!");
+    } else {
+        alert("Incorrect!");
+    }
    }
-
-  else if 
 }
 
 function startQuiz() {
